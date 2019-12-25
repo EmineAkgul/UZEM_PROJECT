64a548797999a13c0f9914d3488e51cf0a6146e5
Emine Akgül <EmineAkgul@github.com>
2019-12-25 21:43:47 +0300

    TheFirstCommit
    The First Commit

---
+++ Uzem_Project/Uzem.Core/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzem.Core
+{
+    public class Class1
+    {
+        //hellö from the other side :) 
+    }
+}
